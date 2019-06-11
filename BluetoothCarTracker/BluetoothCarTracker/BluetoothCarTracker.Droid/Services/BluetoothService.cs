21dad534503e5bc4f6d33b3f9a03e56019e03ce0
Alexander Kvenvolden <kvenclassical@gmail.com>
2019-06-11 08:32:33 -0700

    Create initial Uno project.

---
+++ BluetoothCarTracker/BluetoothCarTracker/BluetoothCarTracker.Droid/Services/BluetoothService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Bluetooth;
+
+namespace BluetoothCarTracker.Droid.Services
+{
+    public class BluetoothService
+    {
+        public BluetoothService()
+        {
+
+        }
+
+        public void Init()
+        {
+            BluetoothAdapter adapter;
+        }
+    }
+}
